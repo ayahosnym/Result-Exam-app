be61a0c43c91ce25e72238ef77567a7bd20c08bb
ayahosnym <ayahosny019@gmail.com>
2020-08-07 01:36:36 +0200

    version2

--- src/components/ExamResults/ExamResults.js
+++ src/components/ExamResults/ExamResults.js
@@ -1,31 +1,33 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./ExamResults.css";
+import "./ExamResults.scss";
 import Navigation from "../Navigation/Navigation";
 
 const currentStudent = JSON.parse(localStorage.getItem("currentStudent"));
-function ExamResults() {
-  const [results, setResult] = useState([]);
 
+function ExamResults() {
+  const [results, setResults] = useState([]);
+
+  // get  all student result then see the current student to show his result
   useEffect(() => {
     axios.get("http://localhost:3000/results").then((response) => {
-      setResult(response.data);
+      setResults(response.data);
     });
   });
 
-  const getResult = results.map((data) => {
-    if (currentStudent) {
-      if (data.id == currentStudent.id) {
-        return <li>congrates your result is : {data.result}</li>;
-      }
+  //display result
+  const getResult = results.map((value) => {
+    if (currentStudent && value.id == currentStudent.id) {
+      
+      return <h3> Your Result is : {value.result} from 1000</h3>;
     }
   });
+
   return (
-    <div>
+    <section id="exam-result">
       <Navigation />
-      <h1> helllo there I am waiting </h1>
-      this is : {getResult}
-    </div>
+      <div className="text-center my-5">{getResult}</div>
+    </section>
   );
 }
 
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,29 +1,25 @@
 import React from "react";
-import "./Navigation.css";
+import "./Navigation.scss";
 import { Link, useHistory } from "react-router-dom";
 
 export default function Navigation() {
   const history = useHistory();
-  
+
   function logout() {
     localStorage.clear();
     history.push("/login");
   }
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="profile">profile</Link>
-          </li>
-          <li>
-            <Link to="/" onClick={logout}>
-              {" "}
-              logout
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+
+    <nav >
+      <ul className="list-unstyled py-3">
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/login">Log Out</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
